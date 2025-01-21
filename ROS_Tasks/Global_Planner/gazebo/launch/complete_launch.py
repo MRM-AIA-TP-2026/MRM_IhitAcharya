5e5cc8c2afda3a0c4ccea36cfb23974934ebd2aa
IhitplayzYT <ihitexe@gmail.com>
2025-01-21 21:48:51 +0530

    ATT_Task

--- ROS_Tasks/Global_Planner/gazebo/launch/complete_launch.py
+++ ROS_Tasks/Global_Planner/gazebo/launch/complete_launch.py
@@ -28,6 +28,7 @@ def generate_launch_description():
     name='joint_state_publisher',
     output='screen'
 )
+    
   
     rvizNode = Node(
         package='rviz2',
--- ROS_Tasks/Global_Planner/gazebo/src/gps.cpp
+++ ROS_Tasks/Global_Planner/gazebo/src/gps.cpp
@@ -22,19 +22,12 @@ public:
         std::cin >> target_lon_;
         std::tie(distance, bearing) = calculateDistanceAndBearing(current_lat_, current_lon_, target_lat_, target_lon_);
         movement_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
-        end_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
         gps_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>("/gps", 10, std::bind(&GpsNavigator::gpsCallback, this, std::placeholders::_1));
-
     }
-
-private:
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr movement_pub_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr end_pub_;
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::TimerBase::SharedPtr inter_timer_;
-
-
 
     void gpsCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg) {
         std::tie(distance, bearing) = calculateDistanceAndBearing(current_lat_, current_lon_, target_lat_, target_lon_);
@@ -43,9 +36,6 @@ private:
         navigateToTarget();
     }
     
-
-
-
     void navigateToTarget() {
         if (bearing == 0)
         {
@@ -54,17 +44,15 @@ private:
         
         if (n == 0)
         {auto twist_msg = geometry_msgs::msg::Twist(); 
-        double time = std::abs(bearing/0.5);
+        n++;
+        double time = std::abs(bearing/0.5)+0.5;
         auto inter = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::duration<double>(time));       
-        twist_msg.angular.z = 0.5;
+        twist_msg.angular.z = std::min(0.5,std::max(-1.0,bearing));
         twist_msg.linear.x = 0.0;
         movement_pub_->publish(twist_msg);
         inter_timer_ = this->create_wall_timer(inter, std::bind(&GpsNavigator::Straight, this));
-        }
-        
-        n++;
+        }    
 }
-
 
 void Straight()
 {       auto twist_msg = geometry_msgs::msg::Twist();
@@ -78,8 +66,6 @@ void Straight()
         twist_msg.angular.z = 0.0;
         movement_pub_->publish(twist_msg);
         timer_ = this->create_wall_timer(interval, std::bind(&GpsNavigator::end, this));
-
-    
 }
 
 void end()
@@ -91,7 +77,6 @@ RCLCPP_INFO(this->get_logger(), "Target Reached\n");
 rclcpp::shutdown();
 exit(0);
 }
-
 
     std::pair<double, double> calculateDistanceAndBearing(double lat1, double lon1, double lat2, double lon2) {
         const double deg_to_rad = M_PI / 180.0;
