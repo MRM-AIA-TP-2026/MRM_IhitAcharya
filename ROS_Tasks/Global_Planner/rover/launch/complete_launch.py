d43d20ed5340e3c7dbd854b4f27df93bfd098608
IhitplayzYT <ihitexe@gmail.com>
2025-01-22 14:15:04 +0530

    ATT task

--- ROS_Tasks/Global_Planner/rover/launch/complete_launch.py
+++ ROS_Tasks/Global_Planner/rover/launch/complete_launch.py
@@ -6,46 +6,65 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
 
+
 def generate_launch_description():
     
     robotXacroname = 'rover'
-    namePackage = 'gazebo'
+    namePackage = 'rover'
     modelFileRelativePath = 'model/rover.xacro'
     worldFileRelativePath = 'model/world.world'
     rvizFileRelativePath = 'rviz/custom_config.rviz'
     pathModelFile = os.path.join(get_package_share_directory(namePackage),modelFileRelativePath)
     pathWorldFile = os.path.join(get_package_share_directory(namePackage),worldFileRelativePath)
+    rvizConfigFile = os.path.join(get_package_share_directory(namePackage),rvizFileRelativePath)
     robotDescription = xacro.process_file(pathModelFile).toxml()
     gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py'))
     gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch,launch_arguments={'world': pathWorldFile}.items())
     spawnModelNode = Node(package='gazebo_ros',executable='spawn_entity.py',arguments=['-topic','robot_description', '-entity',robotXacroname],output = 'screen')
     nodeRobotStatePublisher = Node(package='robot_state_publisher',executable='robot_state_publisher',output = 'screen',parameters=[{'robot_description' : robotDescription,'use_sim_time':True}])
     
+    joint_state_publisher = Node(
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher',
+    output='screen'
+)
     
-    rvizConfigFile = os.path.join(get_package_share_directory(namePackage), rvizFileRelativePath)
+  
     rvizNode = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
-        arguments=['-d', rvizConfigFile]
+        arguments=['-d', rvizConfigFile],
+        parameters=[{'robot_description': robotDescription}]
     )
 
-    # Node to execute the custom C++ file
-    customCppNode = Node(
-        package='gazebo',  # Replace with your package name
-        executable='gps',  # Replace with your C++ executable name
-        name='gpstogps',
-        output='screen'
+    static_transform_publisher = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_odom_tf_broadcaster",
+        arguments=["0", "0", "0", "0", "0", "0", "map", "odom"],
+        parameters=[{'use_sim_time': True}],
     )
-    
-    
+
+    customCppNode = Node(
+        package='rover',  
+        executable='gps',  
+        name='gps',
+         output='screen',
+        parameters=[{'use_sim_time': True}],
+        arguments=['--ros-args', '--log-level', 'info']
+    )
+   
     launchDescriptionObject = LaunchDescription()
     launchDescriptionObject.add_action(gazeboLaunch)
     launchDescriptionObject.add_action(spawnModelNode)
     launchDescriptionObject.add_action(nodeRobotStatePublisher)
     launchDescriptionObject.add_action(rvizNode)
     launchDescriptionObject.add_action(customCppNode)
+    launchDescriptionObject.add_action(joint_state_publisher)
+    launchDescriptionObject.add_action(static_transform_publisher)
 
     return launchDescriptionObject
     
